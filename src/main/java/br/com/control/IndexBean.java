1e09ed8c7e07c4bd1dce5cca4c0a313814c96221
Daniel Soares <danielsoaresf@hotmail.com>
2018-12-01 16:34:42 -0200

    Crud Generico e outras coisas

---
+++ src/main/java/br/com/control/IndexBean.java
@@ -0,0 +1,10 @@
+package br.com.control;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name = "indexBean")
+public class IndexBean {
+
+}
